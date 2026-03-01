ce0a02a5a961b81c430e72839499e00988efcced
Yan Fortes <yan.fortespb@gmail.com>
2026-03-01 17:50:14 -0300

    Feat(0008): Finalizando alguns ajustes.

--- ControleFinanceiro_REST.DTO/Entities/CategoriaDTO.cs
+++ ControleFinanceiro_REST.DTO/Entities/CategoriaDTO.cs
@@ -1,11 +1,16 @@
 ﻿using ControleFinanceiro_REST.DTO.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControleFinanceiro_REST.DTO.Entities;
 
 public class CategoriaDTO
 {
     public Guid Id { get; set; }
+    [Required(ErrorMessage = "Descrição é obrigatória.")]
+    [MaxLength(400, ErrorMessage = "Descrição deve ter no máximo 400 caracteres.")]
     public string Descricao { get; set; } = null!;
+
+    [Required(ErrorMessage = "Finalidade é obrigatória.")]
     public FinalidadeCategoriaEnum Finalidade { get; set; }
     public Guid UsuarioId { get; set; }
     public DateTime DataCriacao { get; set; }
